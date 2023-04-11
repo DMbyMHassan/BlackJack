724c73f13a16f77e5ce63542f5b6e038969af977
mkhassan <mahmoudkamal31@icloud.com>
2023-04-11 03:07:09 +0300

    Initial commit

---
+++ db.py
@@ -0,0 +1,7 @@
+# This model for writing and reading money
+import csv
+def read_money():
+    pass
+
+def write_money():
+    pass
---
+++ main.py
@@ -0,0 +1,25 @@
+ #black project 2023  CNA
+
+
+from decimal import Decimal
+MIN_BET = 5
+MAXIMUM_BIT = 10
+BLACKJACK_PAY = Decimal('1.5')
+
+def get_bet_amount():
+    pass
+
+def get_card_value():
+    pass
+
+def get_hand_points():
+    pass
+
+def main():
+
+if __name__ == '__main__':
+    main()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
