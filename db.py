cbf166e1f7650d8b065eb9ce1b89e1ee78b78e07
mkhassan <mahmoudkamal31@icloud.com>
2023-04-16 17:40:31 +0300

    buliding the main

--- db.py
+++ db.py
@@ -1,7 +1,20 @@
 # This model for writing and reading money
 import csv
 def read_money():
-    pass
+    try:
+        with open('money.txt','r') as file:
+            reader = csv.reader(file)
+            money = next(reader)
+            return float(money[0])
+    except FileNotFoundError:
+        print("Money file not found.Creating new one")
+        write_money(100)
+        return 100
 
-def write_money():
-    pass
+
+def write_money(money):
+    with open('money.txt','w') as file:
+        writer = csv.writer(file)
+        writer.writerow([money])
+
+
--- main.py
+++ main.py
@@ -1,21 +1,51 @@
- #black project 2023  CNA
+import random  #black project 2023  CNA
 
 
 from decimal import Decimal
+import db
+
 MIN_BET = 5
 MAXIMUM_BIT = 10
 BLACKJACK_PAY = Decimal('1.5')
 
-def get_bet_amount():
-    pass
+def get_bet_amount(money):
+    while True:
+        try:
+            bet_amount = Decimal(input(f"Money: {money}\nBet Amount: "))
+            if bet_amount < MIN_BET or bet_amount > MAX_BET:
+                print(f"Invalid bet amount. Please enter a bet between {MIN_BET} and {MAX_BET}.")
+            elif bet_amount > money:
+                print("You don't have enough money to place that bet.")
+            else:
+                return bet_amount
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
-def get_card_value():
-    pass
+
+def get_card_value(card):
+    return [2]
 
 def get_hand_points():
     pass
+def print_cards_dealer():
+    pass
+def print_cards_palyer():
+    pass
+
+
 
 def main():
+    print("BLACKJACK")
+    print("Blackjack payout 3:2")
+    money = db.read_money()
+    while money > 0:
+        db.write_money(money)
+        bet_amount = get_bet_amount(money)
+        random.shuffle(deck)
+
+
+
+
 
 if __name__ == '__main__':
     main()
