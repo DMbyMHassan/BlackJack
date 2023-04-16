7f53565d96f14dd735be4203e2dc7cad7a83e65d
mkhassan <mahmoudkamal31@icloud.com>
2023-04-16 17:47:34 +0300

    buliding the main

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import random  #black project 2023  CNA
-
-
+ #black project 2023  CNA
+import random
 from decimal import Decimal
 import db
 
 MIN_BET = 5
-MAXIMUM_BIT = 10
+MAXIMUM_BIT = 100
 BLACKJACK_PAY = Decimal('1.5')
 
 def get_bet_amount(money):
     while True:
         try:
             bet_amount = Decimal(input(f"Money: {money}\nBet Amount: "))
-            if bet_amount < MIN_BET or bet_amount > MAX_BET:
-                print(f"Invalid bet amount. Please enter a bet between {MIN_BET} and {MAX_BET}.")
+            if bet_amount < MIN_BET or bet_amount > MAXIMUM_BIT:
+                print(f"Invalid bet amount. Please enter a bet between {MIN_BET} and {MAXIMUM_BIT}.")
             elif bet_amount > money:
                 print("You don't have enough money to place that bet.")
             else:
