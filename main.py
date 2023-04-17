453114b54697e488e51eb7650f7ea5ca62fac32e
mkhassan <mahmoudkamal31@icloud.com>
2023-04-17 09:39:25 +0300

    adjusting handling excpetion

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # black project 2023  CNA Mahmoud Hassan
+import decimal
 import random
 from decimal import Decimal
 import db
 import card
+import sys
 
 # definig the min and max bit as global vriable and Blackjack pay
 MIN_BET = 5
@@ -21,8 +23,11 @@ def get_bet_amount(money):
                 print("You don't have enough money to place that bet.")
             else:
                 return bet_amount
-        except ValueError:
+        except decimal.InvalidOperation:
             print("Invalid input. Please enter a number.")
+        except Exception:
+            print("An unexcpected error occured")
+            sys.exit()
 
 
 # This function returns the point value of a card
@@ -50,7 +55,7 @@ def print_cards_dealer(cards, hidden_card=False):
     else:
         print("DEALER'S CARDS:")
         for card in cards:
-            print(f"{card[1]} of{card[0]}")
+            print(f"{card[1]} of {card[0]}")
 
 
 # printing the player cards
@@ -67,7 +72,7 @@ def main():
     # read money from file
     money = db.read_money()
 
-    while money > 0:
+    while money > MIN_BET:
         db.write_money(money)
         bet_amount = get_bet_amount(money)
 
@@ -95,7 +100,7 @@ def main():
             money += bet_amount.quantize(Decimal('0.01')) * BLACKJACK_PAY
         else:
             while True:
-                respons = input("Hit or stand? (hit/stand))")
+                respons = input("Hit or stand? (hit/stand)")
                 if respons.lower() == 'hit':
                     player_hand.append(deck.pop())
                     print_cards_palyer(player_hand)
@@ -131,7 +136,9 @@ def main():
         print("You donot have enough money")
         respons = input("Would you like to buy more chips (y/n): ")
         while respons.lower() == 'y':
-            money = Decimal(input(f"How many chips would like to buy: "))
+            money += Decimal(input(f"How many chips would like to buy: "))
+            while money < 5:
+                money += Decimal(input(f"Add More Money ,AS Minum bit is 5; how much would like to add more?"))
             db.write_money(money)
             bet_amount = get_bet_amount(money)
             deck = card.create_deck()
@@ -147,7 +154,7 @@ def main():
                 money += bet_amount.quantize(Decimal('0.01')) * BLACKJACK_PAY
             else:
                 while True:
-                    respons = input("Hit or stand? (hit/stand))")
+                    respons = input("Hit or stand? (hit/stand): ")
                     if respons.lower() == 'hit':
                         player_hand.append(deck.pop())
                         print_cards_palyer(player_hand)
