88468192707026e8d1b38d049b025c3320e2d475
mkhassan <mahmoudkamal31@icloud.com>
2023-04-17 08:04:47 +0300

    Finalising the main function adjusting card module

---
+++ card.py
@@ -0,0 +1,21 @@
+# this module to define deck cards using list to store suits,rank and points
+
+import random
+
+SUITS = ['Hearts','Diamonds','clubs', 'Speades']
+RANKS = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+VALUE = {'Ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10,
+         'Queen': 10, 'King': 10}
+
+
+def create_deck():
+    deck = []
+    for suit in SUITS:
+        for rank in RANKS:
+            value = VALUE[rank]
+            deck.append([suit, rank, value])
+    return deck
+
+
+def shuffle_deck():
+    random.shuffle()
